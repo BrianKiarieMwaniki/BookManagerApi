eea388f89c7c5f6341fddaab2e019e63e53defb6
BrianKiarieMwaniki <briank.mwaniki@gmail.com>
2022-12-27 17:36:38 +0300

    add a post method

--- routes/books.js
+++ routes/books.js
@@ -7,8 +7,22 @@ router.get("/", async (req, res) => {
   connection.query(q, (err, data) => {
     if (err) return res.json(err);
 
-    return res.data;
+    return res.json(data);
   });
 });
+
+router.post("/", (req,res) =>{
+    const q = "INSERT INTO books (`title`,`desc`,`cover`) VALUES(?)";
+    const values = [
+        req.body.title,
+        req.body.desc,
+        req.body.cover
+    ];
+
+    connection.query(q,[values], (err, data) =>{
+        if(err) return res.json(err);
+        return res.json("Book has been created successfully");
+    })
+})
 
 module.exports = router;
